258150f9e8bf452e92118f2d6b66e839f761aea7
Seiya KUBO <s.kubo@i3design.co.jp>
2019-02-28 12:41:29 +0900

    fix: typo
    calender → calendar

--- i3designKitTests/Core/DateFormatterTests.swift
+++ i3designKitTests/Core/DateFormatterTests.swift
@@ -10,25 +10,25 @@ import XCTest
 
 class DateFormatterTests: XCTestCase {
     
-    let calender = Calendar(identifier: .gregorian)
+    let calendar = Calendar(identifier: .gregorian)
 
     func test_dateFromString() {
-        guard let date = DateFormatter.date(from: "20190214 12:01:30", format: "yyyyMMdd HH:mm:ss", calendar: calender) else {
+        guard let date = DateFormatter.date(from: "20190214 12:01:30", format: "yyyyMMdd HH:mm:ss", calendar: calendar) else {
             return
         }
-        XCTAssertEqual(2019, calender.component(.year, from: date))
-        XCTAssertEqual(2, calender.component(.month, from: date))
-        XCTAssertEqual(14, calender.component(.day, from: date))
-        XCTAssertEqual(12, calender.component(.hour, from: date))
-        XCTAssertEqual(1, calender.component(.minute, from: date))
-        XCTAssertEqual(30, calender.component(.second, from: date))
+        XCTAssertEqual(2019, calendar.component(.year, from: date))
+        XCTAssertEqual(2, calendar.component(.month, from: date))
+        XCTAssertEqual(14, calendar.component(.day, from: date))
+        XCTAssertEqual(12, calendar.component(.hour, from: date))
+        XCTAssertEqual(1, calendar.component(.minute, from: date))
+        XCTAssertEqual(30, calendar.component(.second, from: date))
     }
 
     func test_stringFromDate() {
         let format = "yyyyMMdd HH:mm:ss"
-        guard let date = DateFormatter.date(from: "20190214 12:01:30", format: format, calendar: calender) else {
+        guard let date = DateFormatter.date(from: "20190214 12:01:30", format: format, calendar: calendar) else {
             return
         }
-        XCTAssertEqual("20190214 12:01:30", DateFormatter.string(from: date, format: format, calender: calender))
+        XCTAssertEqual("20190214 12:01:30", DateFormatter.string(from: date, format: format, calendar: calendar))
     }
 }
